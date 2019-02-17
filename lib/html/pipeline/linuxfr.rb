ac0d2c8217ca9cbf1f6f3164ec015eaad8426ec4
Adrien Dorsaz <adrien@adorsaz.ch>
2019-02-17 18:46:52 +0100

    linuxfr: add explicite whitelist (copy from the current default)

--- lib/html/pipeline/linuxfr.rb
+++ lib/html/pipeline/linuxfr.rb
@@ -7,7 +7,30 @@ module HTML
         toc_minimal_length: 5000,
         toc_header: "<h2 class=\"sommaire\">Sommaire</h2>\n",
         svgtex_url: "http://localhost:16000",
-        host: "linuxfr.org"
+        host: "linuxfr.org",
+        whitelist: {
+          :elements => %w(a abbr b blockquote br cite code dd del dfn div dl dt em
+            h1 h2 h3 h4 h5 h6 hr i img ins kbd li mark meter ol p pre
+            q s samp small source span strong sub sup table tbody td
+            tfooter th thead tr time ul var video wbr),
+          :remove_contents => ['script'],
+          :attributes => {
+            :all         => ['data-after', 'data-id', 'id', 'title', 'class'],
+            'a'          => ['href', 'name'],
+            'blockquote' => ['cite'],
+            'img'        => ['alt', 'height', 'src', 'width'],
+            'q'          => ['cite'],
+            'source'     => ['src', 'type', 'media'],
+            'time'       => ['datetime'],
+            'video'      => ['src', 'controls']
+          },
+          :protocols => {
+            'a'          => {'href' => ['ftp', 'http', 'https', 'irc', 'mailto', 'xmpp', 'ed2k', 'magnet', :relative]},
+            'blockquote' => {'cite' => ['http', 'https', :relative]},
+            'img'        => {'src'  => ['http', 'https', :relative]},
+            'q'          => {'cite' => ['http', 'https', :relative]}
+          }
+        }
       }
 
       def self.render(text)
